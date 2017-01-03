221f3a48ce49546bef42069afc69621a957ea49c
willoo <shenliwei@fudan.edu.cn>
2017-01-03 16:04:18 +0800

    PC

--- loadhead.js
+++ loadhead.js
@@ -16,6 +16,7 @@ if (level==1) {
 
 	var venueurl = "venue.html";
 	var orgcommitteeurl = "Organization Committee.html";
+	var procommitteeurl = "Program Committee.html";
 }
 else {
 	var indexurl = "../index.html";
@@ -31,6 +32,7 @@ else {
 
 	var venueurl = "../venue.html";
 	var orgcommitteeurl = "../Organization Committee.html";
+	var procommitteeurl = "../Program Committee.html";
 }
 
 var navbarheader = $('<div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="'+indexurl+'">ICSME 2017</a></div>');
@@ -69,6 +71,7 @@ navbarcollapseul.append(li_venue);
 var li_team = $('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Team<span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></li>');
 li_team.find('ul').append('<li><a href="http://conferences.computer.org/icsm/Steering_Committee_Members.html">Steering Committee</a></li>');
 li_team.find('ul').append('<li><a href="'+orgcommitteeurl+'">Organizing Committee</a></li>');
+li_team.find('ul').append('<li><a href="'+procommitteeurl+'">Program Committee</a></li>');
 navbarcollapseul.append(li_team);        
 
 navbarcollapse.append(navbarcollapseul);
