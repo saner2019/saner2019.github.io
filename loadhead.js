7aaa7ccc12134508f6ac5625bcfaf3e2cda53de1
willoo <shenliwei@fudan.edu.cn>
2017-06-19 11:43:39 +0800

    add visa

--- loadhead.js
+++ loadhead.js
@@ -22,8 +22,10 @@ if (level==1) {
 	var venueurl = "venue.html";
 	var orgcommitteeurl = "Organization Committee.html";
 	var procommitteeurl = "Program Committee.html";
+
+	var travelvisaurl = "travel/visareq.html";
 }
-else if (level==2) {
+else if (level>=2) {
 	var indexurl = "../index.html";
 	var Registrationurl = "../ToBeAnnounced.html";
 	var Programurl = "../ToBeAnnounced.html";
@@ -32,27 +34,29 @@ else if (level==2) {
 	var PaperAccepted = "../program/Accepted.html";
 	var LGBT = "../program/LGBT.html";
 
-	var ResearchTrackCFP = "ResearchTrackCFP.html";
-	var ERATrackCFP = "NIERTrackCFP.html";
-	var IndustryTrackCPF = "IndustryTrackCFP.html";
-	var ToolDemoTrackCFP = "ToolDemoCFP.html";
-	var DoctoralCFP = "DoctoralSymposiumCFP.html";
-	var ArtifactsTrackCFP="ArtifactsTrackCFP.html";
-	var CallForJournalFirstPresentation="CallForJournalFirstPresentation.html"; 
+	var ResearchTrackCFP = "../cfp/ResearchTrackCFP.html";
+	var ERATrackCFP = "../cfp/NIERTrackCFP.html";
+	var IndustryTrackCPF = "../cfp/IndustryTrackCFP.html";
+	var ToolDemoTrackCFP = "../cfp/ToolDemoCFP.html";
+	var DoctoralCFP = "../cfp/DoctoralSymposiumCFP.html";
+	var ArtifactsTrackCFP="../cfp/ArtifactsTrackCFP.html";
+	var CallForJournalFirstPresentation="../cfp/CallForJournalFirstPresentation.html"; 
 
 	var venueurl = "../venue.html";
 	var orgcommitteeurl = "../Organization Committee.html";
 	var procommitteeurl = "../Program Committee.html";
-}
 
+	var travelvisaurl = "../travel/visareq.html";
+}
+/**
 else if (level==3) {
 	var indexurl = "../index.html";
 	var Registrationurl = "../ToBeAnnounced.html";
 	var Programurl = "../ToBeAnnounced.html";
 	var Socialurl = "../ToBeAnnounced.html";
 
-	var PaperAccepted = "Accepted.html";
-	var LGBT = "LGBT.html";
+	var PaperAccepted = "../program/Accepted.html";
+	var LGBT = "../program/LGBT.html";
 
 	var ResearchTrackCFP = "../cfp/ResearchTrackCFP.html";
 	var ERATrackCFP = "../cfp/NIERTrackCFP.html";
@@ -66,6 +70,7 @@ else if (level==3) {
 	var orgcommitteeurl = "../Organization Committee.html";
 	var procommitteeurl = "../Program Committee.html";
 }
+**/
 
 var navbarheader = $('<div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="'+indexurl+'">ICSME 2017</a></div>');
 navpartcontainer.append(navbarheader);
@@ -111,6 +116,10 @@ li_team.find('ul').append('<li><a href="http://conferences.computer.org/icsm/Ste
 li_team.find('ul').append('<li><a href="'+orgcommitteeurl+'">Organizing Committee</a></li>');
 li_team.find('ul').append('<li><a href="'+procommitteeurl+'">Program Committee</a></li>');
 navbarcollapseul.append(li_team);        
+
+var li_travel = $('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Travel<span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></li>');
+li_travel.find('ul').append('<li><a href="'+travelvisaurl+'">Visa Information</a></li>');
+navbarcollapseul.append(li_travel); 
 
 navbarcollapse.append(navbarcollapseul);
 
