fb63ef6ca6c90af01d0a9ceb6d4401933fed6ded
saner2019 <npudgl@gmail.com>
2018-02-28 19:41:06 +0800

    add loadhead.js

---
+++ common/loadhead.js
@@ -0,0 +1,77 @@
+function loadheaderexecute(level) {
+    var navpart = $('<nav class="navbar navbar-default" role="navigation"></nav>');
+    var navpartcontainer = $('<div class="container" id="id_navcontainer"></div>');
+
+    if (level == 1) {
+        var indexurl = "index.html";
+
+    }
+    else if (level >= 2) {
+        var indexurl = "../index.html";
+
+    }
+
+
+    var navbarheader = $('<div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="' + indexurl + '">SANER 2019</a></div>');
+    navpartcontainer.append(navbarheader);
+
+    var navbarcollapse = $('<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div>');
+    var navbarcollapseul = $('<ul class="nav navbar-nav navbar-right" id="id_navbarul"></ul>');
+
+    navbarcollapse.append(navbarcollapseul);
+
+    navpartcontainer.append(navbarcollapse);
+
+    navpart.append(navpartcontainer);
+
+    $('body').append(navpart);
+
+};
+
+function loadCarousel(level) {
+
+    if (level == 1) {
+        var slide1 = "./img_system/tradition1.png";
+        var slide2 = "./img_system/002.jpg";
+        var slide3 = "./img_system/003.jpg";
+    } else {
+        var slide1 = "../img_system/tradition1.png";
+        var slide2 = "../img_system/002.jpg";
+        var slide3 = "../img_system/003.jpg";
+    }
+    var carousel = $('<div class="container">\n' +
+        '    <div id="myCarousel" class="carousel slide">\n' +
+        '        <!-- 轮播（Carousel）指标 -->\n' +
+        '        <ol class="carousel-indicators">\n' +
+        '            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>\n' +
+        '            <li data-target="#myCarousel" data-slide-to="1"></li>\n' +
+        '            <li data-target="#myCarousel" data-slide-to="2"></li>\n' +
+        '        </ol>\n' +
+        '        <!-- 轮播（Carousel）项目 -->\n' +
+        '        <div class="carousel-inner">\n' +
+        '            <div class="item active">\n' +
+        '                <img src="' + slide1 + '" alt="First slide">\n' +
+        '            </div>\n' +
+        '            <div class="item">\n' +
+        '                <img src="' + slide2 + '" alt="Second slide">\n' +
+        '            </div>\n' +
+        '            <div class="item">\n' +
+        '                <img src="' + slide3 + '" alt="Third slide">\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </div>\n' +
+        '</div>');
+
+    $('body').append(carousel)
+
+    $(document).ready(function () {
+        $('#myCarousel').carousel({interval: 3000});//每隔5秒自动轮播
+    });
+
+};
+
+function test(level) {
+
+};
+
+
